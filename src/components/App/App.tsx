9e60c5dbbdccf3929463f345ce8d4b74425c6178
Inna Bondarenko <bondarenko211287@gmail.com>
2025-05-21 22:46:20 +0200

    components

---
+++ src/components/App/App.tsx
@@ -0,0 +1,14 @@
+// import axios from "axios";
+// import { useState } from "react";
+// import css from "./App.module.css";
+import SearchBar from "../SearchBar/SearchBar";
+
+function App() {
+  return (
+    <>
+      <SearchBar />
+    </>
+  );
+}
+
+export default App;
