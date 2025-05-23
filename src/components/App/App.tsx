bf41c6c6240ddc9c8a9f9bdfc4de851ad485ae38
Inna Bondarenko <bondarenko211287@gmail.com>
2025-05-23 14:14:11 +0200

    loader

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,20 +1,54 @@
 import axios from "axios";
-// import { useState } from "react";
+import { useState } from "react";
 // import css from "./App.module.css";
 import SearchBar from "../SearchBar/SearchBar";
-// import type { Movie } from "../../types/movie";
+import type { Movie } from "../../types/movie";
+import MovieGrid from "../MovieGrid/MovieGrid";
+import Loader from "../Loader/Loader";
+
+interface GetMoviesRes {
+  results: Movie[];
+}
 
 function App() {
-  const handleSearch = async (newQuery: string) => {
-    // console.log("hjhjh", newQuery);
+  const [movies, getMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    // const API_KEY = import.meta.env.VITE_TMDB_API_KEY;&api_key=${API_KEY}
-    const res = await axios.get(
-      `https://api.themoviedb.org/3/search/movie?query=${newQuery}`
-    );
-    console.log("hhhhh", res);
+  const handleSearch = async (newQuery: string) => {
+    const BEARER_TOKEN = import.meta.env.VITE_TMDB_BEARER;
+
+    try {
+      setIsLoading(true);
+
+      const res = await axios.get<GetMoviesRes>(
+        `https://api.themoviedb.org/3/search/movie?query=${newQuery}`,
+        {
+          headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`,
+          },
+        }
+      );
+      getMovies(res.data.results);
+      //   console.log("Результати:", );
+    } catch (error) {
+      console.error("Помилка при запиті:", error);
+    } finally {
+      setIsLoading(false);
+    }
   };
-  return <>{<SearchBar onSearch={handleSearch} />}</>;
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <MovieGrid
+          movies={movies}
+          onSelect={(movie) => console.log("Selected movie:", movie)}
+        />
+      )}
+    </>
+  );
 }
 
 export default App;
